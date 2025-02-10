4da5ca7b2ff0baf37fe083e9b892e951669e7036
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2025-02-11 01:04:30 +0300

    feat: display data - type, time, employee, source, rating

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
+import { useEffect, useState } from "react";
 import { useFetchCallList } from "./api/api";
+import { CallListResponse } from "./types";
+import { incomingCallsArrow, outgoingCallsArrow } from "./components/svg";
 import "./App.css";
 
 function App() {
   const { data, loading, error } = useFetchCallList(
-    "2024-01-01",
-    "2024-01-04",
+    "2025-01-01",
+    "2025-01-04",
     "both"
-  );
+  ) as { data: CallListResponse; loading: boolean; error: string | null };
+
+  const [grades, setGrades] = useState<{ [key: number]: string }>({});
+
+  const formatPhoneNumber = (phone: string) => {
+    if (phone.length !== 11 || phone[0] !== "7") {
+      return phone;
+    }
+    return `+7 (${phone.slice(1, 4)}) ${phone.slice(4, 7)}-${phone.slice(
+      7,
+      9
+    )}-${phone.slice(9)}`;
+  };
+
+  const getRandomGrade = () => {
+    const gradesArray = ["Отлично", "Хорошо", "Плохо"];
+    return gradesArray[Math.floor(Math.random() * gradesArray.length)];
+  };
+
+  const getGradeClass = (grade: string) => {
+    switch (grade) {
+      case "Отлично":
+        return "grade-excellent";
+      case "Хорошо":
+        return "grade-good";
+      case "Плохо":
+        return "grade-bad";
+      default:
+        return "";
+    }
+  };
 
   const formatDuration = (duration: number) => {
     const minutes = Math.floor(duration / 60);
@@ -16,6 +49,16 @@ function App() {
       "0"
     )}`;
   };
+
+  useEffect(() => {
+    if (data?.results) {
+      const initialGrades = data.results.reduce((acc, _, index) => {
+        acc[index] = getRandomGrade();
+        return acc;
+      }, {} as { [key: number]: string });
+      setGrades(initialGrades);
+    }
+  }, [data]);
 
   return (
     <>
@@ -28,29 +71,60 @@ function App() {
         <div className="title-font main-part">
           {loading && <p>Загрузка данных...</p>}
           {error && <p>Ошибка: {error}</p>}
-          {data && data.calls && Array.isArray(data.calls) ? (
+          {data && data.results && Array.isArray(data.results) ? (
             <table>
               <thead>
                 <tr>
+                  <th>Тип</th>
                   <th>Время</th>
                   <th>Сотрудник</th>
-                  <th>Статус</th>
-                  <th>Номер звонящего</th>
-                  <th>Номер звонка</th>
+                  <th>Звонок</th>
+                  <th>Источник</th>
+                  <th>Оценка</th>
                   <th>Длительность</th>
                 </tr>
               </thead>
               <tbody>
-                {data.calls.map((call, index) => (
-                  <tr key={index}>
-                    <td>{new Date(call.date).toLocaleString()}</td>
-                    <td>{call.candidate_name}</td>
-                    <td>{call.status}</td>
-                    <td>{call.from_number}</td>
-                    <td>{call.to_number}</td>
-                    <td>{formatDuration(call.duration)}</td>
-                  </tr>
-                ))}
+                {data.results.map((call, index) => {
+                  const grade = grades[index];
+                  return (
+                    <tr key={call.id}>
+                      <td>
+                        {call.in_out === "1" ? (
+                          <span
+                            dangerouslySetInnerHTML={{
+                              __html: incomingCallsArrow,
+                            }}
+                          />
+                        ) : (
+                          <span
+                            dangerouslySetInnerHTML={{
+                              __html: outgoingCallsArrow,
+                            }}
+                          />
+                        )}
+                      </td>
+                      <td>
+                        {new Date(call.date).toLocaleTimeString("ru-RU", {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                          hour12: false,
+                        })}
+                      </td>
+                      <td>
+                        <img src={call.person_avatar} alt="Avatar" />
+                      </td>
+                      <td>{formatPhoneNumber(call.partner_data.phone)}</td>
+                      <td>{call.partner_data.name}</td>
+                      <td>
+                        <span className={`grade ${getGradeClass(grade)}`}>
+                          {grade}
+                        </span>
+                      </td>
+                      <td>{formatDuration(call.duration)}</td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           ) : (
--- src/types.tsx
+++ src/types.tsx
@@ -1,16 +1,31 @@
+interface PartnerData {
+  id: string;
+  name: string;
+  phone: string;
+}
+
 interface CallRecord {
+  in_out: string;
   id: number;
   date: string;
+  candidate_id: number;
   candidate_name: string;
-  duration: number;
-  status: string;
+  contact_name: string;
   from_number: string;
   to_number: string;
+  status: string;
+  person_name: string;
+  person_surname: string;
+  person_avatar: string;
+  partner_data: PartnerData;
+  duration: number;
 }
 
 export interface CallListResponse {
-  calls: CallRecord[];
-  total: number;
-  page: number;
-  per_page: number;
+  duration: number;
+  results: CallRecord[];
 }
+
+export type InOutCalls = {
+  in_out: "1" | "0" | "";
+};
